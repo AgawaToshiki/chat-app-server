0519d613253c0cb3c088f1b57fcd3905af34886e
AgawaToshiki <t.agawa1128ht@gmail.com>
2023-12-28 19:58:20 +0900

    originを変更

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [`${process.env.ALLOWED_ORIGIN}`]
+    origin: [`${process.env.ALLOWED_ORIGIN}` || "https://chat-app-client-rose.vercel.app"]
   }
 });
 
